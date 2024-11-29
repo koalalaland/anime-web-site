582fed2a3ddc8bd966ba3ff2d97f4761e2a0202e
koalalaland <175555686+koalalaland@users.noreply.github.com>
2024-11-29 17:15:42 +0100

    files

---
+++ anime hwa/ANIMEHWA.js
@@ -0,0 +1,13 @@
+function mesurerTaillePage() {
+    console.log('Hauteur de la page:', document.documentElement.scrollHeight, 'px');
+    console.log('Largeur de la page:', document.documentElement.scrollWidth, 'px');
+}
+
+// Appel de la fonction pour mesurer la taille de la page
+mesurerTaillePage();
+
+function mafonction(){
+   alert("hello world");
+
+}
+mafonction();
